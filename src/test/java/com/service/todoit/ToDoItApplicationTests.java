93e4cf8070e795f79f2e5ee415a9c5098310d647
pyjhoop <pyjhoop@naver.com>
2024-03-12 17:45:54 +0900

    :tada: "Create TodoIt Service"
    - TodoIt 서비스를 위한 프로젝트 생성

---
+++ src/test/java/com/service/todoit/ToDoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.todoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
